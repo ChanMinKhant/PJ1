40a69facd33da0aea908ea75662efb7d81d842b3
La Pyae Htun <lapyaehtun369@gmail.com>
2024-02-08 14:42:46 +0630

    home1

--- frontend/src/pages/auth/Home.jsx
+++ frontend/src/pages/auth/Home.jsx
@@ -36,45 +36,14 @@ return (
              <div className="m-right">
              
 
-             <Link to='/register'><button className='homebtn'>Register Now</button></Link>
-             {/* <div className="proton">
-              < img src={myImg} alt="" className='wheel1'/>
-                  <div className="electron">
-                  
-                     <div className='electron-1'></div>
-                     <div className='electron-2'></div>
-                     <div className='electron-3'></div>
-                  </div>
-               </div>  */}
+            
              
-             {/* < img src={myImg} alt="" className='wheel2'/>
-             < img src={myImg} alt="" className='wheel3'/>  */}
              </div> 
          </div>
     
          
      </div>
-     {/* <div className='aboutUs'>
-     <div className='aboutUsleft'>
-        <div>
-        <h2>About Us</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam consequatur rerum sequi, optio fugiat consectetur unde adipisci? Expedita recusandae minus commodi ea at culpa aperiam fugiat magnam officiis earum.</p>
-        <br />
-        <h2>How to use?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus totam perferendis ullam minus rerum debitis odit reprehenderit ipsam! Dolor animi vel voluptatem. Suscipit velit iure amet corrupti. Soluta, deserunt laudantium?</p>
-        <h2>Step by step guide</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus est itaque consequuntur, suscipit quisquam ducimus nisi non molestias magnam quam animi enim cum, earum asperiores incidunt delectus? Dignissimos, minima hic?</p>
-        </div>
-        
-     </div>
-     <div className="aboutUsright">
-      <Loading/>
-      <svg height={"100%"} width={"200%"}>
-         <circle cx={"100%"} cy={"30%"} r={"100%"}
-         stroke='black' stroke-width={"3px"} fill='navy'/>
-      </svg>
-     </div>
-  </div> */}
+
   </div>
     
 )
