aa9ca26e9a89cf89d378917caeef42aaa526239e
La Pyae Htun <lapyaehtun369@gmail.com>
2024-02-12 20:28:59 +0630

    Add About Us page layout

--- frontend/src/pages/aboutUs/AboutUs.jsx
+++ frontend/src/pages/aboutUs/AboutUs.jsx
@@ -5,9 +5,15 @@ import './aboutUs.css';
 
 const AboutUs = () =>{
     return(
-        <div>
+        <div className='AboutContainer'> 
             <Nav />
-            
+            <div className='sub-container'>
+                <div className='item-1'></div>
+                <div className='item-2'></div>
+                <div className='item-3'></div>
+                <div className='itme-4'></div>
+                <div className='footer'></div>
+            </div>
         </div>
     )
 }
