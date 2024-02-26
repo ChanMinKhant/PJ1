eee2ccc7f2aebb886cbcff05811ff9158ffc30f5
Chan Min Khant <jiyonfire@gmail.com>
2024-02-26 17:16:13 +0630

    Export functions in sendInfoService.js

--- frontend/src/services/sendInfoService.js
+++ frontend/src/services/sendInfoService.js
@@ -2,7 +2,7 @@ import apiService from './apiService';
 
 const authBaseUrl = '/sharing';
 
-const AdminUpload = async (userData) => {
+export const AdminUpload = async (userData) => {
   try {
     const response = await apiService.post(`${authBaseUrl}/upload`, userData);
     return response.data;
@@ -11,7 +11,7 @@ const AdminUpload = async (userData) => {
   }
 };
 
-const sendEmail = async (data) => {
+export const sendEmail = async (data) => {
   try {
     const response = await apiService.post(`${authBaseUrl}/send`, {
       year: 'First',
@@ -23,7 +23,7 @@ const sendEmail = async (data) => {
   }
 };
 
-const getStudents = async (year, semester, major, section) => {
+export const getStudents = async (year, semester, major, section) => {
   //getStudents('First', 'First', 'Computer Science', 'A')
   try {
     const response = await apiService.post(`${authBaseUrl}/students`, {
@@ -38,16 +38,18 @@ const getStudents = async (year, semester, major, section) => {
   }
 };
 
-const deleteStudent = async (id) => {
-  try {
-    const response = await apiService.delete(`${authBaseUrl}/students/${id}`);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+export const deleteStudent = async (id) => {
+  const deleteStudent = async (id) => {
+    try {
+      const response = await apiService.delete(`${authBaseUrl}/students/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
 };
 
-const updateStudent = async (id, data) => {
+export const updateStudent = async (id, data) => {
   try {
     const response = await apiService.put(
       `${authBaseUrl}/students/${id}`,
@@ -58,5 +60,3 @@ const updateStudent = async (id, data) => {
     throw error;
   }
 };
-
-export { AdminUpload, sendEmail };
