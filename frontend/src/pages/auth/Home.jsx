c0042c76536eb35449525d45acf336289232483d
La Pyae Htun <lapyaehtun369@gmail.com>
2024-02-28 22:59:27 +0630

    Update CSS and JSX for home page

--- frontend/src/pages/auth/Home.jsx
+++ frontend/src/pages/auth/Home.jsx
@@ -6,26 +6,30 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='color'>
+    <div className="color">
       <Nav />
-      <div className='home'>
-        <div className='mainHome'>
-          <div className='m-left'>
-            <div className='animate-text'>
+      <div className="home">
+        <div className="mainHome">
+          <div className="m-left">
+            <div className="animate-text">
               <h1>
                 <span></span>
               </h1>
             </div>
-            
-            <Link to='/register'>
-              <button className='homebtn'>Register Now</button>
+
+            <Link to="/register">
+              <button className="homebtn">Register Now</button>
             </Link>
           </div>
-          <div className='m-right'>
-            {/* <div className='circle'>
-              <div className='logo'> </div>
-              <div className='text'></div>
-            </div> */}
+          <div className="m-right">
+            <div className="flex-container">
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+            </div>
           </div>
         </div>
       </div>
